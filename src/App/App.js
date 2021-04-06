00f564ef5623a1adcc718222b2f966714f8127a6
olexii <omedvedew@ukr.net>
2021-04-06 15:12:57 +0300

    connected sass

--- src/App/App.js
+++ src/App/App.js
@@ -1,85 +1,15 @@
 import React from 'react';
-import CSVReader from 'react-csv-reader'; 
-import { connect } from 'react-redux';
-import CSVItem from './CSVItem';
-import { keys } from 'lodash';
+import Main from './Main';
 
-import '../common/csv.css';
+import '../common/style.css';
 
-const App = ({
-  state,
-  getCSVInfo,
-}) => {
-
-  console.log(state);
-
-  // Converting data from CSV file to array.
-  state.CSVInfo ?
-  state.CSVInfo.forEach((item, i) => {
-    state.CSVItems[`item${i}`] = item.join().split("; ")
-  })
-  : console.log('waiting for info');
- 
-  // Validating table content
-  function validateCell() {
-    let rows = document.querySelectorAll('tr');
-    console.log(rows);
-    for (let i=1; i<rows.length-1; i++) {
-      if (rows[i].cells[3].textContent.includes('@') === false) {rows[i].cells[3].classList.add("invalid")};
-      if (+rows[i].cells[4].textContent <= 20) {rows[i].cells[4].classList.add("invalid")};
-      if (+rows[i].cells[5].textContent > +rows[i].cells[4].textContent || +rows[i].cells[5].textContent < 0) {rows[i].cells[5].classList.add("invalid")};
-      if (+rows[i].cells[6].textContent > 1000000) {rows[i].cells[6].classList.add("invalid")};
-      if (+rows[i].cells[8].textContent.length > 2) {rows[i].cells[8].textContent = rows[i].cells[8].textContent.slice(0, 2)};
-      if (+rows[i].cells[2].textContent < 12 || rows[i].cells[2].textContent.startsWith("+1") === false) {rows[i].cells[2].classList.add("invalid")};
-      if (rows[i].cells[7].textContent.toLowerCase() === 'true' || rows[i].cells[7].textContent.toLowerCase() === 'false') {continue} else {rows[i].cells[7].classList.add("invalid")};
-    };
-  };
-  setTimeout(validateCell, 500);
-  
-  return (
-    <>
-      <CSVReader onFileLoaded={(data, fileInfo) => getCSVInfo(data)}/>
-      
-      {
-        state.CSVInfo ?
-        <table border="1" cellSpacing="0" cellPadding="0">
-          <tbody>
-            <tr>
-              <td>ID</td>
-              <td>Name</td>
-              <td>Phone</td>
-              <td>Email</td>
-              <td>Age</td>
-              <td>Experience</td>
-              <td>Yearly Income</td>
-              <td>Has children</td>
-              <td>License States</td>
-              <td>Expiration date</td>
-              <td>License number</td>
-            </tr>
-            {keys(state.CSVItems).map((data, i) => (
-              <tr key={i}>
-                <td>{i+1}</td>
-                <CSVItem data={state.CSVItems[data]}/>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        : console.log('waitin for info')
-      }
-    </>
-  )
+const App = () => {
+    return (
+        <>
+            <h1 className="document-title">Radency test task</h1>
+            <Main/>
+        </>
+    )
 };
 
-const mapStateToProps = (state) => ({
-  state,
-});
-
-const mapDispatchToProps = dispatch => ({
-  getCSVInfo: (value) => dispatch({
-    type: "ADD_INFO_FROM_CSV",
-    value,
-  })
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App/App';
 import { createStore } from 'redux';
+import { mainReducer } from './redux/mainReducer';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { mainReducer } from './redux/mainReducer';
+import App from './App/App';
 
 const store = createStore(
   mainReducer,
